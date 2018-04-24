9d9610f5a9b597b0f0ff349ce2254100912e0dcb
mnap <learn@10g.pl>
2018-04-24 14:19:06 +0200

    Fix code style

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,12 @@ import React from 'react';
 import style from './Todo.css';
 
 const Todo = props => (
-        <li
-            className={style.Todo}
-            onClick={() => {props.action(props.task.id)}}
-        >
-            {props.task.text}
-        </li>
-    );
+    <li
+        className={style.Todo}
+        onClick={() => {props.action(props.task.id)}}
+    >
+        {props.task.text}
+    </li>
+);
 
 export default Todo;
