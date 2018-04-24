f35b75637cf3eac57791d05d5d677a8da4a6d8e2
mnap <learn@10g.pl>
2018-04-24 14:04:35 +0200

    Add todo form import and rendering

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import {hot} from 'react-hot-loader';
 import style from './App.css';
 import Title from '../components/Title';
 import TodoList from '../components/TodoList';
+import TodoForm from '../containers/TodoForm';
 
 class App extends React.Component {
     constructor(props) {
@@ -36,6 +37,9 @@ class App extends React.Component {
                 <Title
                     title={appTitle}
                     tasks={this.state.data.length}
+                />
+                <TodoForm
+                    submit={val => {this.addTodo(val)}}
                 />
                 <TodoList
                     list={this.state.data}
