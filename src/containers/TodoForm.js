8e265ea759cf9b87406359a3a8867a2ccc103cee
mnap <learn@10g.pl>
2018-04-24 13:50:43 +0200

    Add todo form container

---
+++ src/containers/TodoForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import style from './TodoForm.css';
+
+class TodoForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        const {value} = this.state;
+        if (value != '') {
+            this.props.submit(value);
+        }
+    }
+    render() {
+        const formCaption = 'Add task:';
+        return (
+            <form
+                className={style.TodoForm}
+                onSubmit={event => this.handleSubmit(event)}
+            >
+                <label
+                    className={style.TodoForm__label}
+                    htmlFor='taskText'
+                >
+                    {formCaption}
+                </label>
+                <input
+                    className={style.TodoForm__input}
+                    type='text'
+                    id='taskText'
+                    onChange={event => this.handleChange(event)}
+                    value={this.state.value}
+                />
+            </form>
+        );
+    }
+}
+
+export default TodoForm;
